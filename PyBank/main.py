a5678537d3a7c10981e544877025e8d84a7a9f6c
evan-woodard <129635277+evan-woodard@users.noreply.github.com>
2023-09-12 18:18:03 -0500

    push

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,33 +8,37 @@ with open(csv_path) as csvfile:
    csvreader = csv.reader(csvfile, delimiter=",")
    header = next(csvreader)
 
-
    months = []
    profits = []
-
+   iter_profits = iter(profits)
+   change = []
 
    for row in csvreader:
        months.append(row[0])
        profits.append(int(row[1]))
-
+    
+   for i in iter_profits:
+        x = next(iter_profits)
+        change1 = i-x
+        change.append(change1)
 
 results = dict(zip(months, profits))
 
-
 months_count = len(results)
 total_profits = sum(profits)
-#change_average = 
-#change_best = 
-#change_worst = 
+change_average = sum(change)/len(change)
+change_best = max(change)
+change_worst = min(change)
 
 
 print("Financial Analysis")
 print("----------------------------------------------")
 print("Total Months:", months_count)
 print("Total: $", total_profits)
-#print("Average Change: $", change_average)
-#print("Greatest Increase in Profits: $", change_best)
-#print("Greatest Decrease in Profits: $", change_worst)
+print("Average Change: $", round(change_average, 2))
+print("Greatest Increase in Profits: $", change_best)
+print("Greatest Decrease in Profits: $", change_worst)
+
 
 
 #lines = ["Financial Analysis", "----------------------------------------------", ("Total Months:", months_count), ("Total: $", total_profits), \
